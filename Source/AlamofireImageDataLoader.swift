5433efc28417e06cbc08284946a56119b7c05227
kean <grebenyuk.alexander@gmail.com>
2016-03-04 13:07:29 +0300

    Update docs

--- Source/AlamofireImageDataLoader.swift
+++ Source/AlamofireImageDataLoader.swift
@@ -6,16 +6,27 @@ import Foundation
 import Alamofire
 import Nuke
 
+/** Implements image data loading using Alamofire framework.
+ */
 public class AlamofireImageDataLoader: ImageDataLoading {
+    /** The Alamofire.Manager that the receiver was initialized with.
+     */
     public let manager: Alamofire.Manager
-    
+
+    /**
+     Initializes the receiver with a given Alamofire.Manager. Default value is Alamofire.Manager.sharedInstance.
+     
+     - warning: The receiver sets of the Alamofire.Manager startRequestsImmediately to false.
+     */
     public init(manager: Alamofire.Manager = Alamofire.Manager.sharedInstance) {
         manager.startRequestsImmediately = false
         self.manager = manager
     }
     
     // MARK: ImageDataLoading
-    
+
+    /** Creates a request using Alamofire.Manager and returns an NSURLSessionTask which is managed by Alamofire.Manager.
+     */
     public func taskWith(request: ImageRequest, progress: ImageDataLoadingProgress, completion: ImageDataLoadingCompletion) -> NSURLSessionTask {
         let task = self.manager.request(request.URLRequest).response { (_, response, data, error) -> Void in
             completion(data: data, response: response, error: error)
@@ -24,11 +35,15 @@ public class AlamofireImageDataLoader: ImageDataLoading {
         }
         return task.task
     }
-    
+
+    /** Invalidates NSURLSession that Alamofire.Manager was initializd with.
+     */
     public func invalidate() {
         self.manager.session.invalidateAndCancel()
     }
-    
+
+    /** Removes all cached responses from NSURLSession's URL cache.
+     */
     public func removeAllCachedImages() {
         self.manager.session.configuration.URLCache?.removeAllCachedResponses()
     }
