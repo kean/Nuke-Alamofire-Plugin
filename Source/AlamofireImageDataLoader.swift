b47da2e161313e9179eda0a0507668cbd39b8292
kean <grebenyuk.alexander@gmail.com>
2016-07-18 23:29:51 +0300

    Update for Nuke 4 (dev1)

--- Source/AlamofireImageDataLoader.swift
+++ Source/AlamofireImageDataLoader.swift
@@ -12,7 +12,7 @@ public class AlamofireImageDataLoader: ImageDataLoading {
     /** The Alamofire.Manager that the receiver was initialized with.
      */
     public let manager: Alamofire.Manager
-
+    
     /**
      Initializes the receiver with a given Alamofire.Manager. Default value is Alamofire.Manager.sharedInstance.
      
@@ -22,10 +22,10 @@ public class AlamofireImageDataLoader: ImageDataLoading {
         manager.startRequestsImmediately = false
         self.manager = manager
     }
-
+    
     /**
      Initializes the receiver with a Alamofire.Manager created with a given session configuration.
-
+     
      - warning: The receiver sets of the Alamofire.Manager startRequestsImmediately to false.
      */
     public convenience init(configuration: NSURLSessionConfiguration) {
@@ -33,24 +33,25 @@ public class AlamofireImageDataLoader: ImageDataLoading {
     }
     
     // MARK: ImageDataLoading
-
+    
     /** Creates a request using Alamofire.Manager and returns an NSURLSessionTask which is managed by Alamofire.Manager.
      */
     public func taskWith(request: ImageRequest, progress: ImageDataLoadingProgress, completion: ImageDataLoadingCompletion) -> NSURLSessionTask {
-        let task = self.manager.request(request.URLRequest).response { (_, response, data, error) -> Void in
-            completion(data: data, response: response, error: error)
-        }.progress { (_, totalBytesReceived, totalBytesExpected) -> Void in
-            progress(completed: totalBytesReceived, total: totalBytesExpected)
+        let task = self.manager.request(request.URLRequest)
+        task.response { _, response, data, error in
+            completion(task: task.task, data: data, response: response, error: error)
+            }.progress { (_, totalBytesReceived, totalBytesExpected) -> Void in
+                progress(task: task.task, completed: totalBytesReceived, total: totalBytesExpected)
         }
         return task.task
     }
-
+    
     /** Invalidates NSURLSession that Alamofire.Manager was initializd with.
      */
     public func invalidate() {
         self.manager.session.invalidateAndCancel()
     }
-
+    
     /** Removes all cached responses from NSURLSession's URL cache.
      */
     public func removeAllCachedImages() {
