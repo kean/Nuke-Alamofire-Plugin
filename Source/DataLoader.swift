0ca2e401ab5e80731b35d1459b8f0bb6e7a29273
kean <grebenyuk.alexander@gmail.com>
2016-07-24 01:15:19 +0300

    Udpate for new DataLoading protocol; Rename AlamofireImageDataLoader to simple DataLoader

--- Source/DataLoader.swift
+++ Source/DataLoader.swift
@@ -8,11 +8,11 @@ import Nuke
 
 /** Implements image data loading using Alamofire framework.
  */
-public class AlamofireImageDataLoader: Nuke.DataLoading {
+public class DataLoader: Nuke.DataLoading {
     /** The Alamofire.Manager that the receiver was initialized with.
      */
     public let manager: Alamofire.Manager
-
+    
     /**
      Initializes the receiver with a given Alamofire.Manager. Default value is Alamofire.Manager.sharedInstance.
      
@@ -21,10 +21,10 @@ public class AlamofireImageDataLoader: Nuke.DataLoading {
     public init(manager: Alamofire.Manager = Alamofire.Manager.sharedInstance) {
         self.manager = manager
     }
-
+    
     /**
      Initializes the receiver with a Alamofire.Manager created with a given session configuration.
-
+     
      - warning: The receiver sets of the Alamofire.Manager startRequestsImmediately to false.
      */
     public convenience init(configuration: URLSessionConfiguration) {
@@ -32,18 +32,18 @@ public class AlamofireImageDataLoader: Nuke.DataLoading {
     }
     
     // MARK: DataLoading
-
+    
     /** Creates a request using Alamofire.Manager and returns an NSURLSessionTask which is managed by Alamofire.Manager.
      */
-    public func loadData(for request: URLRequest, progress: Nuke.DataLoadingProgress, completion: Nuke.DataLoadingCompletion) -> Cancellable {
+    public func loadData(for request: URLRequest, progress: Nuke.DataLoadingProgress? = nil, completion: Nuke.DataLoadingCompletion) -> Cancellable {
         let task = self.manager.request(request).response { _, response, data, error in
             if let data = data, let response: URLResponse = response {
                 completion(result: .success((data, response)))
             } else {
                 completion(result: .failure(error ?? NSError(domain: NSURLErrorDomain, code: NSURLErrorUnknown, userInfo: nil)))
             }
-        }.progress { (_, totalBytesReceived, totalBytesExpected) -> Void in
-            progress(completed: totalBytesReceived, total: totalBytesExpected)
+            }.progress { (_, totalBytesReceived, totalBytesExpected) -> Void in
+                progress?(completed: totalBytesReceived, total: totalBytesExpected)
         }
         return task.task
     }
